ee38b015f0956de8ff0e2942893dd7c6516859c1
Thomas Gray <tomalexander1999@googlemail.com>
2022-04-18 21:41:11 +0100

    Initial Commit

---
+++ Cook Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cook Book
+//
+//  Created by Thomas Gray on 18/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cook Book/Cook_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cook_BookApp.swift
+//  Cook Book
+//
+//  Created by Thomas Gray on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Cook_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
